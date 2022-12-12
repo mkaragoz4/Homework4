7a6f1a7d278de001000eb82bfa5b2bc4f3c0ba31
M.Ferhat KARAGÖZ <mkaragoz4@gmail.com>
2022-12-12 03:41:24 +0100

    ekleme

---
+++ HomeWorkWeek2/src/hw2/Hw2Escape.java
@@ -0,0 +1,33 @@
+package hw2;
+
+public class Hw2Escape {
+	
+	public static void main(String[]args) {
+	
+	
+		/*
+		 *\t Tab atlamak için kullanılır. 
+		 *\' Metin içerisine tek tırnak karakteri eklemek için kullanılır .
+		 *\" Metne çift tırnak karakteri eklemek için kullanılır .
+		 *\r Satır başı yapmak için kullanılır
+		 *\\ Bu noktada metne ters eğik çizgi karakteri eklemek için kullanılır . 
+		 *\n Bir satır atlar
+		 *\f Bu noktada metne bir form beslemesi eklemek için * kullanılır .
+		 *\b Önceki karakteri silmek için kullanılır . 
+		 */
+		
+		System.out.println("Ali\tbak");
+		System.out.println("Ali\'nin ati");
+		System.out.println("Ali\"ata\"bak");
+		System.out.println("Ali bak \r Ali ata bak");
+		System.out.println("Ali bak\\ ali ata bak");
+		System.out.println("Ali bak \n Ali ata bak");
+		System.out.println("Ali bak \f Ali ata bak");
+		System.out.println("Ali bak\b ali ata bak");
+		
+		
+		
+		
+	}
+
+}
